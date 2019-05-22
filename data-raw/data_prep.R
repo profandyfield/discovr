2832d463002f0ef655ff088f71e02c1374669fc8
Andy Field <andyf@sussex.ac.uk>
2019-05-22 19:31:59 +0100

    add data files

---
+++ data-raw/data_prep.R
@@ -0,0 +1,33 @@
+## code to prepare `DATASET` dataset goes here
+
+library(tidyverse)
+library(here)
+
+get_data <- function(name){
+  tib <- paste0("data-raw/", name) %>%
+    here::here() %>%
+    readr::read_csv()
+  return(tib)
+}
+#########
+
+
+
+# Method of teaching
+
+teaching <- get_data("method_of_teaching.csv") %>%
+  dplyr::mutate(
+    method = forcats::as_factor(method),
+    sex = forcats::as_factor(sex)
+  )
+
+usethis::use_data(teaching)
+
+# Oxoby (2008)
+
+acdc <- get_data("oxoby_2008.csv") %>%
+  dplyr::mutate(
+    singer = forcats::as_factor(singer)
+  )
+
+usethis::use_data(acdc)

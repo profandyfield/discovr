f3ebb6b15f6d7fecc451f19aad3ef8972df5678c
Andy Field <profandyfield@gmail.com>
2019-09-23 20:02:47 +0100

    remove stuff added by github merge

--- R/discovr_data.R
+++ R/discovr_data.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #' Oxoby (2008) data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
@@ -54,8 +54,6 @@
 #' @source \url{https://www.discoveringstatistics.com/books/discovering-statistics-using-r/}
 
 "exam_anxiety"
-=======
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 
 #' Goat or dog data
 #'
@@ -69,15 +67,12 @@
 #' The data contains the following variables:
 #'
 #' \itemize{
-<<<<<<< HEAD
 #'   \item \strong{wife}: whether the person married a goat or a dog
 #'   \item \strong{animal}: how much the person likes animals
 #'   \item \strong{life_satisfaction}: the person's life satisfaction score
-=======
 #'   \item \strong{wife}: Whether the person married a goat or a dog
 #'   \item \strong{animal}: How much the person likes animals
 #'   \item \strong{life_satisfaction}: The person's life satisfaction score
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' }
 #' @format A tibble with 20 rows and 3 variables.
 #' @docType data
@@ -85,7 +80,6 @@
 
 "animal_bride"
 
-<<<<<<< HEAD
 #' Jiminy Cricket data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
@@ -95,7 +89,7 @@
 #' I wonder if the seismic increase in youth internalizing disorders (Twenge, 2000, 2011) is, in part, caused by millions of Disney children reaching the rather depressing realization that ‘wishing upon a star’ didn’t work.
 #' Anyway, imagine that I collected some data from 250 people on their level of success using a composite measure involving their salary, quality of life and how closely their life matches their aspirations.
 #' This gave me a score from 0 (complete failure) to 100 (complete success). I then implemented an intervention: I told people that for the next 5 years they should either wish upon a star for their dreams to come true or work as hard as they could to make their dreams come true.
-#' I measured their success again 5 years later. People were randomly allocated to these two instructions.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      .
+#' I measured their success again 5 years later. People were randomly allocated to these two instructions.
 #' The data contains the following variables:
 #'
 #' \itemize{
@@ -110,9 +104,6 @@
 
 "jiminy_cricket"
 
-
-=======
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' Johns et al. (2012) data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
@@ -125,19 +116,16 @@
 #' pictures from 0 (unattractive) to 100 (attractive). These are the data from that study. The data contains the following variables:
 #'
 #' \itemize{
-<<<<<<< HEAD
 #'   \item \strong{partners}: sexual experience coded as a factor ('Very little' and 'Some")
 #'   \item \strong{pale_pink}: male rating of the attractiveness of pale pink coloured female geneitalia from 0 to 100
 #'   \item \strong{light_pink}: male rating of the attractiveness of light pink coloured female geneitalia from 0 to 100
 #'   \item \strong{dark_pink}: male rating of the attractiveness of dark pink coloured female geneitalia from 0 to 100
 #'   \item \strong{red}: male rating of the attractiveness of red coloured female geneitalia from 0 to 100
-=======
 #'   \item \strong{partners}: Sexual experience coded as a factor ('Very little' and 'Some")
 #'   \item \strong{pale_pink}: Male rating of the attractiveness of pale pink coloured female geneitalia from 0 to 100
 #'   \item \strong{light_pink}: Male rating of the attractiveness of light pink coloured female geneitalia from 0 to 100
 #'   \item \strong{dark_pink}: Male rating of the attractiveness of dark pink coloured female geneitalia from 0 to 100
 #'   \item \strong{red}: Male rating of the attractiveness of red coloured female geneitalia from 0 to 100
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' }
 #' @format A tibble with 40 rows and 5 variables.
 #' @docType data
@@ -154,7 +142,6 @@
 #' The data contains the following variables:
 #'
 #' \itemize{
-<<<<<<< HEAD
 #'   \item \strong{name}: the band member's name
 #'   \item \strong{birth_date}: the band member's date of birth
 #'   \item \strong{death_date}: the band member's date of death (where applicable)
@@ -163,7 +150,6 @@
 #'   \item \strong{songs_written}: the number of songs the band member has contributed to
 #'   \item \strong{net_worth}: the band member's net worth as of 2019 according to some dodgy website
 #'   \item \strong{worth_per_song}: the members net worth per song contributed to
-=======
 #'   \item \strong{name}: The band member's name
 #'   \item \strong{birth_date}: The band member's date of birth
 #'   \item \strong{death_date}: The band member's date of death (where applicable)
@@ -172,7 +158,6 @@
 #'   \item \strong{songs_written}: The number of songs the band member has contributed to
 #'   \item \strong{net_worth}: The band member's net worth as of 2019 according to some dodgy website
 #'   \item \strong{worth_per_song}: The members net worth per song contributed to
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' }
 #' @format A tibble with 7 rows and 8 variables.
 #' @docType data
@@ -180,14 +165,13 @@
 
 "metallica"
 
-<<<<<<< HEAD
 #' The notebook data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
 #'
 #' Fictitious data about the film The Notebook. Imagine that a film company director was interested in whether there was really such a thing as a ‘chick flick’
 #' (a film that has the stereotype of appealing to women more than to men). He took 20 men and 20 women and showed half of each sample a film that was supposed to be a ‘chick flick’ (The Notebook).
-#' The other half watched a documentary about notebooks as a control. In all cases the company director measured participants’ arousal  as an indicator of how much they enjoyed the film.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     .
+#' The other half watched a documentary about notebooks as a control. In all cases the company director measured participants’ arousal  as an indicator of how much they enjoyed the film.
 #' The data contains the following variables:
 #'
 #' \itemize{
@@ -262,26 +246,28 @@
 
 "ong_tidy"
 
-
-
-#' Method of teaching data
+#' Shopping and exercise data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
 #'
-#' The data show the score (out of 20) for 20 different students, some of whom are male and some female,
-#' and some of whom were taught using positive reinforcement (being nice) and others who were taught using punishment (electric shock)
+#' According to some highly unscientific research done by a UK department store chain and reported in Marie Claire magazine (\url{http://ow.ly/9Dxvy}), shopping is good for you.
+#' They found that the average woman spends 150 minutes and walks 2.6 miles when she shops, burning off around 385 calories. In contrast, men spend only about 50 minutes shopping, covering 1.5 miles.
+#' This was based on strapping a pedometer on a mere 10 participants. Although I don’t have the actual data, some simulated data based on these means are in this file.
 #'
 #' \itemize{
-#'   \item \strong{method}: the type of teaching method used
 #'   \item \strong{sex}: biological sex of the individual
-#'   \item \strong{mark}: the score out of 20 on a test
+#'   \item \strong{distance}: the distance travelled in miles
+#'   \item \strong{time}: the time spent shopping in minutes
+#'   \item \strong{sex}: Biological sex of the individual
+#'   \item \strong{distance}: The distance travelled in miles
+#'   \item \strong{time}: The time spent shopping in minutes
 #' }
-#' @format A tibble with 20 rows and 3 variables.
+#' @format A tibble with 10 rows and 3 variables.
 #' @docType data
 #' @source \url{https://www.discoveringstatistics.com/books/discovering-statistics-using-r/}
 
-"teaching"
-=======
+"shopping"
+
 #' Method of teaching data
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
@@ -300,65 +286,6 @@
 
 "teaching"
 
-#' Oxoby (2008) data
-#'
-#' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
-#'
-#' AC/DC are one one of the best-selling hard rock bands in history, with around 100 million certified sales, and an estimated 200 million actual sales.
-#' In 1980 their original singer Bon Scott died of alcohol poisoning and choking on his own vomit. He was replaced by Brian Johnson who has been their singer ever since.
-#' Debate rages with unerring frequency within the rock music press over who is the better frontman. The conventional wisdom is that Bon Scott was better although personally, and I seem to be somewhat in the minority here, I prefer Brian Johnson.
-#' Anyway, Robert Oxoby in a playfull paper decided to put this argument to bed once and for all (Oxoby, 2008).
-#' Using a task from experimental economics called the ultimatum game, individuals are assigned the role of either proposer or responder and paired randomly.
-#' Proposers are allocated $10 from which they have to make a financial offer to the responder (i.e., $2). The responder can accept or reject this offer. If the offer is rejected neither party gets any money,
-#' but if the offer is accepted the responder keeps the offered amount (e.g., $2), and the proposer keeps the original amount minus what they offered (e.g., $8). For half of the participants the song
-#' ‘It’s a long way to the top’ sung by Bon Scott was playing in the background, for the remainder ‘Shoot to thrill’ sung by Brian Johnson was playing. Oxoby measured the offers made by proposers,
-#' and the minimum offers that responders accepted (called the minimum acceptable offer). He reasoned that people would accept lower offers and propose higher offers when listening to something they like
-#' (because of the ‘feel-good factor’ the music creates). Therefore, by comparing the value of offers made and the minimum acceptable offers in the two groups he could see whether people have more of a feel
-#' good factor when listening to Bon or Brian. There were 18 people per group.
-#'
-#' These data are approximated from graphs within Oxoby (2008). The object contains the following variables:
-#'
-#' \itemize{
-#'   \item \strong{singer}: The type of teaching method used
-#'   \item \strong{offer}: Offer made (in dollars)
-#'   \item \strong{mao}: The minimum acceptable offer, MAO, in dollars
-#' }
-#' @docType data
-#' @format A tibble with 36 rows and 2 variables.
-#' @source \url{https://www.discoveringstatistics.com/books/discovering-statistics-using-r/}
-#' @references
-#' \itemize{
-#'    \item Oxoby, R. J. (2008). On the efficiency of AC/DC: Bon Scott versus Brian Johnson. \emph{Economic Enquiry}, \emph{47}, 598-602. \url{https://doi.org/10.1111/j.1465-7295.2008.00138.x}
-#'}
-
-"acdc"
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
-
-#' Shopping and exercise data
-#'
-#' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
-#'
-#' According to some highly unscientific research done by a UK department store chain and reported in Marie Claire magazine (\url{http://ow.ly/9Dxvy}), shopping is good for you.
-#' They found that the average woman spends 150 minutes and walks 2.6 miles when she shops, burning off around 385 calories. In contrast, men spend only about 50 minutes shopping, covering 1.5 miles.
-#' This was based on strapping a pedometer on a mere 10 participants. Although I don’t have the actual data, some simulated data based on these means are in this file.
-#'
-#' \itemize{
-<<<<<<< HEAD
-#'   \item \strong{sex}: biological sex of the individual
-#'   \item \strong{distance}: the distance travelled in miles
-#'   \item \strong{time}: the time spent shopping in minutes
-=======
-#'   \item \strong{sex}: Biological sex of the individual
-#'   \item \strong{distance}: The distance travelled in miles
-#'   \item \strong{time}: The time spent shopping in minutes
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
-#' }
-#' @format A tibble with 10 rows and 3 variables.
-#' @docType data
-#' @source \url{https://www.discoveringstatistics.com/books/discovering-statistics-using-r/}
-
-"shopping"
-
 #' Tea data (small sample)
 #'
 #' A dataset from Field, A. P. (2020). Discovering statistics using R and RStudio (2nd ed.). London: Sage.
@@ -370,13 +297,10 @@
 #'
 
 #' \itemize{
-<<<<<<< HEAD
 #'   \item \strong{tea}: the number of cups of tea a person drinks per day
 #'   \item \strong{cog_fun}: cognitive functioning (out of 80)
-=======
 #'   \item \strong{tea}: The number of cups of tea a person drinks per day
 #'   \item \strong{cog_fun}: Cognitive functioning (out of 80)
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' }
 #' @format A tibble with 15 rows and 2 variables:
 #' @source \url{https://www.discoveringstatistics.com/books/discovering-statistics-using-r/}
@@ -398,17 +322,14 @@
 #' The data are a random subsample of Zhang et al.’s data with the following variables:
 #'
 #' \itemize{
-<<<<<<< HEAD
 #'   \item \strong{id}: participant ID
 #'   \item \strong{sex}: participant's biological sex
 #'   \item \strong{name_type}: the booklet condition to which the participant was allocated: Female fake name, Male fake name or Own name
 #'   \item \strong{accuracy}: the participant's score on the maths test
-=======
 #'   \item \strong{id}: Participant ID
 #'   \item \strong{sex}: Participant's biological sex
 #'   \item \strong{name_type}: The booklet condition to which the participant was allocated: Female fake name, Male fake name or Own name
 #'   \item \strong{accuracy}: The participant's score on the maths test
->>>>>>> 6a2adff7e9b12384dc4c29f43d98f870f6b23049
 #' }
 #' @format A tibble with 52 rows and 4 variables
 #' @docType data

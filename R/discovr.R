17302d0bc73b9bfcc92a5af4652e9fa94924e2f1
Andy <Field>
2020-08-19 10:21:28 +0100

    edit data file info

--- R/discovr.R
+++ R/discovr.R
@@ -54,82 +54,88 @@
 #'
 #' See the book or data descriptions for more details. This is a list of available datasets within the package. Raw CSV files are available from the book's website.
 #'
-#' * [acdc]: Data about whether Bon Scott or Brian Johnson is the best singer of AC/DC. (Also in the file *acdc.csv*.)
-#' * [album_sales]: Fictitious data about predicting album sales from advertising, airplay and the band's image. (Also in the file *album_sales.csv*.)
-#' * [animal_bride]: Fictitious data about life satisfaction when married to a dog or a goat. (Also in the file *animal_bride.csv*.)
-#' * [beckham_1929]: Data from a study by Beckham (1929). (Also in the file *beckham_1929.csv*.)
-#' * [big_hairy_spider]: Fictitious data about whether anxiety is greater after exposure to real spiders or pictures of spiders. (Also in the file *big_hairy_spider.csv*.)
-#' * [biggest_liar]: Fictitious data about creativity and telling lies. (Also in the file *biggest_liar.csv*.)
-#' * [bronstein_2019]: Data about whether delusion proneness predicts belief in fake news because of less analytic thinking. (Also in the file *bronstein_2019.csv*.)
-#' * [bronstein_miss_2019]: The data in [bronstein_2019] but with missing values inserted using MCAR amputation. (Also in the file *bronstein_miss_2019.csv*.)
-#' * [catterplot]: Fictitious data for plotting a catterplot. (Also in the file *catterplot.csv*.)
-#' * [cat_dance]: Fictitious data about training cats to dance. (Also in the file *cat_dance.csv*.)
-#' * [cat_reg]: Fictitious data about training cats to dance. (Also in the file *cat_reg.csv*.)
-#' * [cetinkaya_2006]: data from a study by Cetinkaya and Domjan (2006) about quails with sexual fetishes. Seriously. (Also in the file *cetinkaya_2006.csv*.)
-#' * [chamorro_premuzic]: Data about what students want (personality wise) from their lecturers. (Also in the file *chamorro_premuzic.csv*.)
-#' * [child_aggression]: Fictitious data (based on real research) about predicting aggression in children. (Also in the file *child_aggression.csv*.)
-#' * [coldwell_2006]: Data predicting childhood adjustment from various parenting variables. (Also in the file *coldwell_2006.csv*.)
-#' * [daniels_2012]: Data about the effects of sexualised sports images on self-image. (Also in the file *daniels_2012.csv*.)
-#' * [dark_lord]: Fictitious data about the subliminal messages in songs. (Also in the file *dark_lord.csv*.)
-#' * [download]: Fictitious data about the download music festival and being smelly. (Also in the file *download_festival.csv*.)
-#' * [df_beta]: Fictitious data used to illustrate the DF Beta statistic. (Also in the file *df_beta.csv*.)
-#' * [essay_marks]: Fictitious data about essay marking. (Also in the file *essay_marks.csv*.)
-#' * [exam_anxiety]: Fictitious data about exam performance, anxiety and revision. (Also in the file *exam_anxiety.csv*.)
-#' * [gallup_2003]: Data that tests a hypothesis about why penises have a bell end. (Also in the file *gallup_2003.csv*.)
-#' * [gelman_2009]: Data used to critically evaluate the explanations (and claim) that there are more beautiful women than men in the world. (Also in the file *gelman_2009.csv*.)
-#' * [glastonbury]: More fictitious data about music festivals and being smelly. (Also in the file *glastonbury.csv*.)
-#' * [grades]: Fictitious data about statistics grades. (Also in the file *grades.csv*.)
-#' * [ice_bucket]: Data about the ice bucket challenge. (Also in the file *ice_bucket.csv*.)
-#' * [invisibility_cloak]: Fictitious data about how much mischief peopel would get up to if they had an invisibility cloak. (Also in the file *invisibility.csv*.)
-#' * [invisibility_rm]: Fictitious data about how much mischief peopel would get up to if they had an invisibility cloak but using a repeated measures design. (Also in the file *invisibility_rm.csv*.)
-#' * [hiccups]: Fictitious data on digital rectal stimulation and hiccups. (Also in the file *hiccups.csv*.)
-#' * [honesty_lab]: Fictitious data about perceptions of honesty. (Also in the file *honesty_lab.csv*.)
-#' * [jiminy_cricket]: Fictitious data about whether wishing on a star makes you successful. (Also in the file *jiminy_cricket.csv*.)
-#' * [johns_2012]: Data about whether the colour red is a mating signal to men. (Also in the file *johns_2012.csv*.)
-#' * [lambert_2012]: Data about whether pornography use is related to relationhsip commitment and infidelity. (Also in the file *lambert_2012.csv*.)
-#' * [massar_2012]: Data about whether gossiping has an evolutionary function. (Also in the file *massar_2012.csv*.)
-#' * [mcnulty_2008]: Simulated data to match the results of a study about whether attractivenes sis linked to the support given within a relationship. (Also in the file *mcnulty_2008.csv*.)
-#' * [men_dogs]: Fictitious data about whether men exhibit dog-like behaviours (compared to dogs). (Also in the file *men_dog.csv*.)
-#' * [metal_health]: Fictitious data about whether listening to heavy metal negatively affects mental health. (Also in the file *metal_health.csv*.)
-#' * [metallica]: Data about thrash metal band, Metallica. (Also in the file *metallica.csv*.)
-#' * [murder]: Fictitious data about the number of murder each month at three street locations (Ruskin Avenue, Acacia Avenue and Rue Morgue). (Also in the file *murder.csv*.)
-#' * [notebook]: Fictitious data about whether watching the film the notebook is emotionally arousing. (Also in the file *notebook.csv*.)
-#' * [ong_2011]: Data about social media profile pictures and personality traits. (Also in the file *ong_2011.csv*.)
-#' * [ong_tidy]: Data about social media profile pictures and personality traits. (Also in the file *ong_tidy.csv*.)
-#' * [pubs]: Data illustrating the difference between an outlier and an influential case. (Also in the file *pubs.csv*.)
-#' * [puppies]: Fictitious data related to whether puppy therapy works. (Also in the file *puppies.csv*.)
-#' * [r_exam]: Fictitious data relating to an R exam at two universities. (Also in the file *r_exam.csv*.)
-#' * [roaming_cats]: Fictitious data about how far cats roam from their homes (Also in the file *roaming_cats.csv*.)
-#' * [self_help]: Fictitious data about whether self-help books improve relationship satisfaction. (Also in the file *self_help.csv*.)
-#' * [self_help_dsur]: Fictitious data about whether self-help books improve relationship satisfaction compared to statistics books. (Also in the file *self_help_dsur.csv*.)
-#' * [shopping]: Fictitious data about shopping. (Also in the file *shopping_exercise.csv*.)
-#' * [social_anxiety]: Fictitious (I think) data about whether social anxiety symptoms are specific to social anxiety. (Also in the file *social_anxiety.csv*.)
-#' * [social_media]: Fictitious data about the effects of social media on grammar. (Also in the file *social_media.csv*.)
-#' * [soya]: fictitious data about the effects of eating soya on sperm count. (Also in the file *soya.csv*.)
-#' * [students]: I can't even remember what this data file contains. (Also in the file *student.csv*.)
-#' * [superhero]: fictitious data about whether wearing different superhero costumes leads to more severe physical injuries. (Also in the file *superhero.csv*.)
-#' * [supermodel]: Fictitious data about supermodel salaries. (Also in the file *supermodel.csv*.)
-#' * [tablets]: Fictitious data about predicting the desirability of computing tablets. (Also in the file *tablets.csv*.)
-#' * [tea_15]: Fictitious data based on real data about cognitive functioning and drinking tea. (Also in the file *tea_makes_you_brainy_15.csv*.)
-#' * [tea_716]: Fictitious data based on real data about cognitive functioning and drinking tea. (Also in the file *tea_makes_you_brainy_716.csv*.)
-#' * [teaching]: Fictitious data about the success of different methods of teaching. (Also in the file *teaching.csv*.)
-#' * [teach_method]: More fictitious data about the success of different methods of teaching. (Also in the file *teach_method.csv*.)
-#' * [tumour]: fictitious data about mobile phone use and brain tumours. (Also in the file *tumour.csv*.)
-#' * [video_games]: Fictitious data about the relationship between video game use, callous unemotional traits and aggression. (Also in the file *video_games.csv*.)
-#' * [tuk_2011]: Data about whether needing to urinate helps decision making. (Also in the file *tuk_2011.csv*.)
-#' * [zhang_sample]: Data about whether performing a maths test under a different name assists performance. (Also in the file *zhang_2013_subsample.csv*.)
+#' * [acdc]: Data about whether Bon Scott or Brian Johnson is the best singer of AC/DC.
+#' * [album_sales]: Fictitious data about predicting album sales from advertising, airplay and the band's image.
+#' * [animal_bride]: Fictitious data about life satisfaction when married to a dog or a goat.
+#' * [beckham_1929]: Data from a study by Beckham (1929).
+#' * [big_hairy_spider]: Fictitious data about whether anxiety is greater after exposure to real spiders or pictures of spiders.
+#' * [biggest_liar]: Fictitious data about creativity and telling lies.
+#' * [bronstein_2019]: Data about whether delusion proneness predicts belief in fake news because of less analytic thinking.
+#' * [bronstein_miss_2019]: The data in [bronstein_2019] but with missing values inserted using MCAR amputation.
+#' * [catterplot]: Fictitious data for plotting a catterplot.
+#' * [cat_dance]: Fictitious data about training cats to dance.
+#' * [cat_reg]: Fictitious data about training cats to dance.
+#' * [cetinkaya_2006]: data from a study by Cetinkaya and Domjan (2006) about quails with sexual fetishes. Seriously.
+#' * [chamorro_premuzic]: Data about what students want (personality wise) from their lecturers.
+#' * [child_aggression]: Fictitious data (based on real research) about predicting aggression in children.
+#' * [coldwell_2006]: Data predicting childhood adjustment from various parenting variables.
+#' * [daniels_2012]: Data about the effects of sexualised sports images on self-image.
+#' * [dark_lord]: Fictitious data about the subliminal messages in songs.
+#' * [download]: Fictitious data about the download music festival and being smelly.
+#' * [df_beta]: Fictitious data used to illustrate the DF Beta statistic.
+#' * [elephooty]: Fictitious data about elephants playing football (soccer).
+#' * [essay_marks]: Fictitious data about essay marking.
+#' * [exam_anxiety]: Fictitious data about exam performance, anxiety and revision.
+#' * [gallup_2003]: Data that tests a hypothesis about why penises have a bell end.
+#' * [gelman_2009]: Data used to critically evaluate the explanations (and claim) that there are more beautiful women than men in the world.
+#' * [glastonbury]: More fictitious data about music festivals and being smelly.
+#' * [grades]: Fictitious data about statistics grades.
+#' * [ice_bucket]: Data about the ice bucket challenge.
+#' * [invisibility_base]: Fictitious data about how much mischief people would get up to if they had an invisibility cloak using a pre-post study design.
+#' * [invisibility_cloak]: Fictitious data about how much mischief people would get up to if they had an invisibility cloak using an independent design.
+#' * [invisibility_rm]: Fictitious data about how much mischief people would get up to if they had an invisibility cloak but using a repeated measures design.
+#' * [hangover]: fictitious data about the efficacy of different drinks as cures for a hangover.
+#' * [hiccups]: Fictitious data on digital rectal stimulation and hiccups.
+#' * [honesty_lab]: Fictitious data about perceptions of honesty.
+#' * [jiminy_cricket]: Fictitious data about whether wishing on a star makes you successful.
+#' * [johns_2012]: Data about whether the colour red is a mating signal to men.
+#' * [lambert_2012]: Data about whether pornography use is related to relationship commitment and infidelity.
+#' * [massar_2012]: Data about whether gossiping has an evolutionary function.
+#' * [mcnulty_2008]: Simulated data to match the results of a study about whether attractiveness is linked to the support given within a relationship.
+#' * [men_dogs]: Fictitious data about whether men exhibit dog-like behaviours (compared to dogs).
+#' * [metal_health]: Fictitious data about whether listening to heavy metal negatively affects mental health.
+#' * [metallica]: Data about thrash metal band Metallica.
+#' * [murder]: Fictitious data about the number of murder each month at three street locations (Ruskin Avenue, Acacia Avenue and Rue Morgue).
+#' * [muris_2008]: Data about whether you can train children to interpret ambiguous situations in a particular way.
+#' * [notebook]: Fictitious data about whether watching the film the notebook is emotionally arousing.
+#' * [ong_2011]: Data about social media profile pictures and personality traits.
+#' * [ong_tidy]: Data about social media profile pictures and personality traits.
+#' * [pubs]: Data illustrating the difference between an outlier and an influential case.
+#' * [puppies]: Fictitious data related to whether puppy therapy works.
+#' * [puppy_love]: Fictitious data related to whether puppy therapy works when you adjust for a person's love of puppies.
+#' * [r_exam]: Fictitious data relating to an R exam at two universities.
+#' * [roaming_cats]: Fictitious data about how far cats roam from their homes.
+#' * [self_help]: Fictitious data about whether self-help books improve relationship satisfaction.
+#' * [self_help_dsur]: Fictitious data about whether self-help books improve relationship satisfaction compared to statistics books.
+#' * [shopping]: Fictitious data about shopping.
+#' * [social_anxiety]: Fictitious (I think) data about whether social anxiety symptoms are specific to social anxiety.
+#' * [social_media]: Fictitious data about the effects of social media on grammar.
+#' * [soya]: fictitious data about the effects of eating soya on sperm count.
+#' * [stalker]: fictitious data about therapy for stalking.
+#' * [students]: I can't even remember what this data file contains.
+#' * [superhero]: fictitious data about whether wearing different superhero costumes leads to more severe physical injuries.
+#' * [supermodel]: Fictitious data about supermodel salaries.
+#' * [tablets]: Fictitious data about predicting the desirability of computing tablets.
+#' * [tea_15]: Fictitious data based on real data about cognitive functioning and drinking tea.
+#' * [tea_716]: Fictitious data based on real data about cognitive functioning and drinking tea.
+#' * [teaching]: Fictitious data about the success of different methods of teaching.
+#' * [teach_method]: More fictitious data about the success of different methods of teaching.
+#' * [tumour]: fictitious data about mobile phone use and brain tumours.
+#' * [video_games]: Fictitious data about the relationship between video game use, callous unemotional traits and aggression.
+#' * [tuk_2011]: Data about whether needing to urinate helps decision making.
+#' * [zhang_sample]: Data about whether performing a maths test under a different name assists performance.
 #'
 #' @section Smart Alex solutions:
 #'
-#'Solutions for end of chapter tasks are available at [www.discovr.rocks](https://www.discovr.rocks/solutions/alex/).
+#'Solutions for end of chapter tasks are available at [www.discovr.rocks/solutions/alex/](https://www.discovr.rocks/solutions/alex/).
 #'
 #' @section Labcoat Leni solutions:
 #'
-#'Solutions for the Labcoat Leni tasks are available at [www.discovr.rocks](https://www.discovr.rocks/solutions/leni/).
+#'Solutions for the Labcoat Leni tasks are available at [www.discovr.rocks/solutions/leni/](https://www.discovr.rocks/solutions/leni/).
 #'
 #' @section Chapter code:
 #'
-#'Although I recommend working through the interactive solutions, each book Chapter has online code and a downloadable R Markdown file available from [www.discovr.rocks](https://www.discovr.rocks/solutions/code/).
+#'Although I recommend working through the interactive solutions, each book Chapter has online code and a downloadable R Markdown file available from [www.discovr.rocks/solutions/leni/](https://www.discovr.rocks/solutions/code/).
 #'
 #'
 #' @section References:
